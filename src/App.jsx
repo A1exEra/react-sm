d20f06417b6e5d04b939588e7c713b11e291c1a3
A1exEra <eramisant@yahoo.com>
2023-03-06 21:30:36 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
 
-function App() {
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div className="App">
       <h1>Jacked Up And Good To Go</h1>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
-}
+};
 
 export default App;
